2488f9fa26f62049f788fe93e4f23d0ae58aea42
Moumita Ray <moumitacloud08@gmail.com>
2020-10-30 23:23:40 +0530

    @ConfigurationProperties

--- src/main/java/com/samplemicroservice/springbootsampleconfig/GreetingController.java
+++ src/main/java/com/samplemicroservice/springbootsampleconfig/GreetingController.java
@@ -1,5 +1,6 @@
 package com.samplemicroservice.springbootsampleconfig;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,8 +23,11 @@ public class GreetingController {
     @Value("#{${dbValues}}")
     private Map<String,String> dbValues;
 
+    @Autowired
+    private DbSettings dbSettings;
+
     @GetMapping("/greeting")
     public String greeting() {
-        return greetingMessage + staticMessage+ listValue + dbValues;
+        return dbSettings.getConnection()+ dbSettings.getHost();
     }
 }
