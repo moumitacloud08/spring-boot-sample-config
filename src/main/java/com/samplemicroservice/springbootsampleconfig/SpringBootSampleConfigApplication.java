b67af8a233ec5d69c55cd6c31285b846e4c08046
Moumita Ray <moumitacloud08@gmail.com>
2020-10-30 19:25:06 +0530

    Initial commit

---
+++ src/main/java/com/samplemicroservice/springbootsampleconfig/SpringBootSampleConfigApplication.java
@@ -0,0 +1,13 @@
+package com.samplemicroservice.springbootsampleconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSampleConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSampleConfigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samplemicroservice/springbootsampleconfig/SpringBootSampleConfigApplicationTests.java
@@ -0,0 +1,15 @@
+package com.samplemicroservice.springbootsampleconfig;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+class SpringBootSampleConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
